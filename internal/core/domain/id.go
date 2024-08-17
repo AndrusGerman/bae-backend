44d5aab6f6ca144753c38cee81f95c7cfb40fb76
Andrus Diaz German <andrusdiazaleman@gmail.com>
2024-08-17 14:22:04 -0400

    add: nil

--- internal/core/domain/id.go
+++ internal/core/domain/id.go
@@ -3,3 +3,21 @@ package domain
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Id primitive.ObjectID
+
+func (id Id) MarshalJSON() ([]byte, error) {
+	return primitive.ObjectID(id).MarshalJSON()
+}
+func (id Id) MarshalText() ([]byte, error) {
+	return primitive.ObjectID(id).MarshalText()
+}
+
+func (id Id) Hex() string {
+	return primitive.ObjectID(id).Hex()
+}
+
+func (id Id) UnmarshalText(b []byte) error {
+	var x = primitive.NewObjectID()
+	x.UnmarshalJSON(b)
+
+	return nil
+}
