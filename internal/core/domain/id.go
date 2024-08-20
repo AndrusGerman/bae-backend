6827a69c8d334217dc276f0b9006acaf052a63bc
Andrus Diaz German <andrusdiazaleman@gmail.com>
2024-08-20 12:59:45 -0400

    add: primitive id value

--- internal/core/domain/id.go
+++ internal/core/domain/id.go
@@ -2,7 +2,6 @@ package domain
 
 import (
 	"encoding"
-	"log"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -18,38 +17,31 @@ type Id struct {
 }
 
 func (id Id) MarshalJSON() ([]byte, error) {
-	log.Println("MarshalJSON")
 	return id.value.MarshalJSON()
 }
 func (id Id) MarshalText() ([]byte, error) {
-	log.Println("MarshalText")
 	return id.value.MarshalText()
 }
 
 func (id Id) Hex() string {
-	log.Println("Hex")
 	return id.value.Hex()
 }
 
 func (id Id) String() string {
-	log.Println("String")
 	return id.value.String()
 }
 
-func (id Id) UnmarshalJSON(b []byte) error {
-	log.Println("UnmarshalJSON")
-	return id.value.UnmarshalJSON(b)
-}
-
-func (id Id) UnmarshalText(b []byte) error {
-	log.Println("UnmarshalText")
-	return id.value.UnmarshalText(b)
-}
 func (id Id) IsZero() bool {
-	log.Println("IsZero")
 	return id.value.IsZero()
 }
 func (id Id) Timestamp() time.Time {
-	log.Println("Timestamp")
 	return id.value.Timestamp()
+}
+
+func (id *Id) UnmarshalJSON(b []byte) error {
+	return id.value.UnmarshalJSON(b)
+}
+
+func (id *Id) UnmarshalText(b []byte) error {
+	return id.value.UnmarshalText(b)
 }
--- internal/core/domain/user.go
+++ internal/core/domain/user.go
@@ -1,12 +1,8 @@
 package domain
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type ObjectID primitive.ObjectID
-
 type User struct {
-	Id      ObjectID `json:"Id" bson:"_id"`
-	Country string   `json:"country" bson:"country"`
-	Phone   string   `json:"phone" bson:"phone"`
-	Email   string   `json:"email" bson:"email"`
+	Id      Id     `json:"Id" bson:"_id"`
+	Country string `json:"country" bson:"country"`
+	Phone   string `json:"phone" bson:"phone"`
+	Email   string `json:"email" bson:"email"`
 }
