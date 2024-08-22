3bb1fd6dfbf3ca72a6a6d6292cfc18be3663a576
Andrus Diaz German <andrusdiazaleman@gmail.com>
2024-08-22 19:28:14 -0400

    add: user

--- internal/core/domain/user.go
+++ internal/core/domain/user.go
@@ -1,8 +1,9 @@
 package domain
 
 type User struct {
-	Id       Id      `json:"Id" bson:"_id"`
-	UserName string  `json:"username" bson:"username"`
-	Phone    Phone   `json:"phone" bson:"phone"`
-	Country  Country `json:"country" bson:"countryId"`
+	Id           Id      `json:"Id" bson:"_id"`
+	UserName     string  `json:"username" bson:"username"`
+	ProfileImage string  `json:"profileImage" bson:"profileImage"`
+	Phone        Phone   `json:"phone" bson:"phone"`
+	Country      Country `json:"country" bson:"countryId"`
 }
