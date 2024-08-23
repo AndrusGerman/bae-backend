0956803c5e3ad15b4a1c5f0ad953418c63637345
Andrus Diaz German <andrusdiazaleman@gmail.com>
2024-08-23 08:57:42 -0400

    add: chat user port

--- internal/core/port/chatuser.go
+++ internal/core/port/chatuser.go
@@ -5,12 +5,10 @@ import "bae-backend/internal/core/domain"
 //go:generate mockgen -source=chatuser.go -destination=mock/chatuser.go -package=mock
 
 type ChatUserRepository interface {
-	Create(contact *domain.Chat) (*domain.Chat, error)
-	GetChats(userId domain.Id) ([]domain.Chat, error)
+	GetChats(userId domain.Id) ([]domain.ChatWithUser, error)
 }
 
 // ChatUserService is an interface for interacting with contact-related business logic
 type ChatUserService interface {
-	Create(contact *domain.Chat) (*domain.Chat, error)
-	GetChats(userId domain.Id) ([]domain.Chat, error)
+	GetChats(userId domain.Id) ([]domain.ChatWithUser, error)
 }
